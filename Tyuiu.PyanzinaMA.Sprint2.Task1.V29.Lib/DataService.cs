f918037deac94ea49e81b26aae38507ab202004b
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-10-04 20:58:38 +0500

    Task1

---
+++ Tyuiu.PyanzinaMA.Sprint2.Task1.V29.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.PyanzinaMA.Sprint2.Task1.V29.Lib
+{
+    public class DataService : ISprint2Task1V29
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (a == d) | (a!=c) ;
+            res[1] = (b < a) & (c > b) ;
+            res[2] = (c <= a) || (b >= c);
+            res[3] = (a != d) && (a > b) ;
+            res[4] = !(a == b) ;
+            res[5] = (d >= c) ^ (a < c) ;
+
+            return res;
+        }
+    }
+}
+//  true, true, true, false, true, true
+//  ==, !=, <, >, <=, >=,
+//  |, &, ||, &&, !, ^, последовательность операций не должна нарушаться
+//a = 657;
+//b = 654;
+//c = 657;
+//d = 657;
---
+++ Tyuiu.PyanzinaMA.Sprint2.Task1.V29/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PyanzinaMA.Sprint2.Task1.V29.Lib;
+
+namespace Tyuiu.PyanzinaMA.Sprint2.Task1.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int a = 657;
+            int b = 654;
+            int c = 657;
+            int d = 657;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a,b,c,d);
+
+            Console.Title = "Спринт #2 | Выполнила: Пьянзина М. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Логические операции                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнила: Пьянзина Мария Алексеевна | ИИПб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз в         *");
+            Console.WriteLine("* выражении) и логических операций (|, &, ||, &&, !, ^, последовательность*");
+            Console.WriteLine("* операций не должна нарушаться), а также арифметических выражений,       *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив):                   *");
+            Console.WriteLine("* (True, True, True, False, True, True)                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("* a = 657, b = 654, c = 657, d = 657                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+//Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность можно чередовать, но использовать один раз в выражении) и логических операций (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться), а также арифметических выражений, которая вернет логическую последовательность(массив): (True, True, True, False, True, True), при a = 657, b = 654, c = 657, d = 657
