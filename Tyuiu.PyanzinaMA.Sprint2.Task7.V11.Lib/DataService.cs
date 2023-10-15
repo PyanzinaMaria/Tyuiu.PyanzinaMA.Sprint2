d5eb1afe33961c8189369b75ac339d53c9e0be1e
Tyuiu.PyanzinaMA@outlook.com <mrpnzn@yaandex.ru>
2023-10-16 02:47:08 +0500

    Task7

--- Tyuiu.PyanzinaMA.Sprint2.Task7.V11.Lib/DataService.cs
+++ Tyuiu.PyanzinaMA.Sprint2.Task7.V11.Lib/DataService.cs
@@ -12,13 +12,20 @@ namespace Tyuiu.PyanzinaMA.Sprint2.Task7.V11.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if (((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && (x > 0)) || ((y <= 1) && (y >= x - 1) && (x >= 0)))
+            if ((x<0) && (x * x + y * y <= 1))
             {
                 res = true;
             }
             else
             {
-                res = false;
+                if ((x >= 0) && (y >= x) && (x * x + y * y <= 1))
+                {
+                    res = true;
+                }
+                else
+                {
+                    res = false;
+                }
             }
             return res;
         }
